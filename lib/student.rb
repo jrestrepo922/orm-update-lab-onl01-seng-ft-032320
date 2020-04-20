f8d0237e3677037a5ed9ed9f14a8983def3d056b
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-20 16:01:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,9 +32,9 @@ class Student
 
 
   def save
-    if self.id 
-      
-    else 
+    if self.id
+
+    else
       sql = <<-SQL
         INSERT INTO students(name, grade) VALUES (?,?)
         SQL
@@ -42,7 +42,7 @@ class Student
         DB[:conn].execute(sql, self.name, self.grade)
 
         @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-      end 
+      end
   end
 
 
